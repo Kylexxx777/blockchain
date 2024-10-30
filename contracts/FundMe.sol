23cb3d261353d00bdfa4392cfd44b1b3d46a8b88
Kylexxx777 <yccheng21@gmail.com>
2024-10-30 11:56:20 +0800

    fund me commit

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,16 +5,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe{
 
-    uint256 public number;
+    uint256 public minimumUsd = 50;
 
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this contract?
-        number = 5;
-        require(msg.value >= 1e18, "Didn't send enough!"); // 1e18 = 1000000000000000000
+        require(msg.value >= minimumUsd, "Didn't send enough!"); // 1e18 = 1000000000000000000
     }
+
+    function getPrice() public view returns(uint256){
+        //ABI 
+        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price,,, ) = priceFeed.latestRoundData();
+        //ETH in trms of USD
+        //3000.000000
+        return uint256(price * 1e10);
+    }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate() public {}
 
     // function withdraw(){}
 }
