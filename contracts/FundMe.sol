0ad8d250a331bcb1628d6cf07a0d3d3d849d6eef
Kylexxx777 <yccheng21@gmail.com>
2024-11-16 21:17:22 +0800

    fund me done

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -14,6 +14,11 @@ contract FundMe{
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
@@ -26,6 +31,7 @@ contract FundMe{
     }
 
     function withdraw() public {
+        // for each owner -> require(msg.sender == owner, "Sender isn't owner!");
         // for loop // start index, ending index, step amount
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
@@ -34,10 +40,18 @@ contract FundMe{
         funders = new address[](0); // reset the array
         // actually withdraw the funds
         // transfer // msg.sender = adress // payable(msg.sender) = payable address
-        payable(msg.sender).transfer(address(this).balance);
-        // send 
-        bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        require(sendSuccess, "Send failed");
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send 
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
 
+    // modifier -> many owners
+    modifier onlyOwner{
+        require(msg.sender == owner, "Sender isn't owner!");
+        _; // do other codes in withdraw() afterwards
     }
 }
